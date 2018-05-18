3738dd19a09cd20aea81dd0dbc6ba3945982a7c7
shahratin <shahratin@gmail.com>
2018-05-18 21:45:39 +0600

    Initial commit

---
+++ verse/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace verse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.myCustomNumericBox2 = new verse.MyCustomNumericBox();
+            this.myCustomNumericBox1 = new verse.MyCustomNumericBox();
+            ((System.ComponentModel.ISupportInitialize)(this.myCustomNumericBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myCustomNumericBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(260, 177);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Previous";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(233, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(128, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(37, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Go";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "আল ফাতিহা",
+            "আল বাক্বারাহ",
+            "আল ইমরান",
+            "আন নিসা",
+            "আল মায়েদাহ",
+            "আল আন-আম",
+            "আল আ’রাফ",
+            "আল-আনফাল",
+            "আত তাওবাহ",
+            "ইউনুস",
+            "হুদ",
+            "ইউসূফ",
+            "রা’দ",
+            "ইব্রাহীম",
+            "হিজর",
+            "নাহল",
+            "বনী ইসরাঈল",
+            "কাহফ",
+            "মারইয়াম",
+            "ত্বোয়া-হা",
+            "আম্বিয়া",
+            "হাজ্জ্ব",
+            "আল মু’মিনূন",
+            "আন-নূর",
+            "আল-ফুরকান",
+            "আশ-শো’আরা",
+            "নমল",
+            "আল কাসাস",
+            "আল আনকাবুত",
+            "আর-রূম",
+            "লোকমান",
+            "সেজদাহ",
+            "আল আহযাব",
+            "সাবা",
+            "ফাতির",
+            "ইয়াসীন",
+            "আস-সাফফাত",
+            "ছোয়াদ",
+            "আল-যুমার",
+            "আল-মু’মিন",
+            "হা-মীম সেজদাহ",
+            "আশ-শুরা",
+            "যুখরুফ",
+            "আদ দোখান",
+            "আল জাসিয়া",
+            "আল আহক্বাফ",
+            "মুহাম্মদ",
+            "আল ফাতহ",
+            "আল হুজরাত",
+            "ক্বাফ",
+            "আয-যারিয়াত",
+            "আত্ব তূর",
+            "আন-নাজম",
+            "আল ক্বামার",
+            "আর রহমান",
+            "আল ওয়াক্বিয়া",
+            "আল হাদীদ",
+            "আল মুজাদালাহ",
+            "আল হাশর",
+            "আল মুমতাহিনা",
+            "আছ-ছফ",
+            "আল জুমুআহ",
+            "মুনাফিকুন",
+            "আত-তাগাবুন",
+            "আত্ব-ত্বালাক্ব",
+            "আত-তাহরীম",
+            "আল মুলক",
+            "আল কলম",
+            "আল হাক্বক্বাহ",
+            "আল মা’আরিজ",
+            "নূহ",
+            "আল জিন",
+            "মুযযামমিল",
+            "আল মুদ্দাসসির",
+            "আল ক্বেয়ামাহ",
+            "আদ-দাহর",
+            "আল মুরসালাত",
+            "আন-নাবা",
+            "আন-নযিআ’ত",
+            "আবাসা",
+            "আত-তাকভীর",
+            "আল ইনফিতার",
+            "আত-তাতফীফ",
+            "আল ইনশিক্বাক্ব",
+            "আল বুরূজ",
+            "আত্ব-তারিক্ব",
+            "আল আ’লা",
+            "আল গাশিয়াহ",
+            "আল ফজর",
+            "আল বালাদ",
+            "আশ-শামস",
+            "আল লায়ল",
+            "আদ্ব-দ্বোহা",
+            "আল ইনশিরাহ",
+            "ত্বীন",
+            "আলাক",
+            "কদর",
+            "বাইয়্যিনাহ",
+            "যিলযাল",
+            "আদিয়াত",
+            "কারেয়া",
+            "তাকাসূর",
+            "আছর",
+            "হুমাযাহ",
+            "ফীল",
+            "কোরাইশ",
+            "মাউন",
+            "কাওসার",
+            "কাফিরুন",
+            "নছর",
+            "লাহাব",
+            "এখলাছ",
+            "ফালাক্ব",
+            "নাস"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 196);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(260, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // myCustomNumericBox2
+            // 
+            this.myCustomNumericBox2.Font = new System.Drawing.Font("Inconsolata", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myCustomNumericBox2.Location = new System.Drawing.Point(72, 221);
+            this.myCustomNumericBox2.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.myCustomNumericBox2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.myCustomNumericBox2.Name = "myCustomNumericBox2";
+            this.myCustomNumericBox2.Size = new System.Drawing.Size(50, 25);
+            this.myCustomNumericBox2.TabIndex = 6;
+            this.myCustomNumericBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.myCustomNumericBox2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.myCustomNumericBox2.ValueChanged += new System.EventHandler(this.myCustomNumericBox2_ValueChanged_1);
+            // 
+            // myCustomNumericBox1
+            // 
+            this.myCustomNumericBox1.Font = new System.Drawing.Font("Inconsolata", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myCustomNumericBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.myCustomNumericBox1.Location = new System.Drawing.Point(12, 221);
+            this.myCustomNumericBox1.Maximum = new decimal(new int[] {
+            114,
+            0,
+            0,
+            0});
+            this.myCustomNumericBox1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.myCustomNumericBox1.Name = "myCustomNumericBox1";
+            this.myCustomNumericBox1.Size = new System.Drawing.Size(54, 25);
+            this.myCustomNumericBox1.TabIndex = 5;
+            this.myCustomNumericBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.myCustomNumericBox1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.myCustomNumericBox1.ValueChanged += new System.EventHandler(this.myCustomNumericBox1_ValueChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 255);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.myCustomNumericBox2);
+            this.Controls.Add(this.myCustomNumericBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.myCustomNumericBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myCustomNumericBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private MyCustomNumericBox myCustomNumericBox1;
+        private MyCustomNumericBox myCustomNumericBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ verse/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace verse
+{
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = Program.Get.verse(1,1);
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void myCustomNumericBox1_ValueChanged_1(object sender, EventArgs e)
+        {
+            myCustomNumericBox2.Maximum = Program.Get.vCount(myCustomNumericBox1.Value);
+            myCustomNumericBox2.Value = 1;
+            textBox1.Text = Program.Get.verse(myCustomNumericBox1.Value, myCustomNumericBox2.Value);
+            comboBox1.SelectedIndex = (int)myCustomNumericBox1.Value - 1;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (myCustomNumericBox2.Value <= 1)
+            {
+                if (myCustomNumericBox1.Value > 1)
+                {
+                    myCustomNumericBox1.Value--;
+                    myCustomNumericBox2.Value = Program.Get.vCount(myCustomNumericBox1.Value);
+                    textBox1.Text = Program.Get.verse(myCustomNumericBox1.Value, myCustomNumericBox2.Value);
+                }
+            }
+            else
+            {
+                myCustomNumericBox2.Value--;
+                textBox1.Text = Program.Get.verse(myCustomNumericBox1.Value, myCustomNumericBox2.Value);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(myCustomNumericBox2.Value >= Program.Get.vCount(myCustomNumericBox1.Value)){
+                if (myCustomNumericBox1.Value < 114) {
+                    myCustomNumericBox1.Value++;
+                    myCustomNumericBox2.Value = 1;
+                    textBox1.Text = Program.Get.verse(myCustomNumericBox1.Value, myCustomNumericBox2.Value);
+                }
+            }
+            else
+            {
+                    myCustomNumericBox2.Value++;
+                    textBox1.Text = Program.Get.verse(myCustomNumericBox1.Value, myCustomNumericBox2.Value);
+            }
+           
+
+        }
+
+        private void myCustomNumericBox2_ValueChanged_1(object sender, EventArgs e)
+        {
+            textBox1.Text = Program.Get.verse(myCustomNumericBox1.Value, myCustomNumericBox2.Value);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+                textBox1.Text = Program.Get.verse(myCustomNumericBox1.Value, myCustomNumericBox2.Value);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCustomNumericBox1.Value = comboBox1.SelectedIndex + 1;
+            myCustomNumericBox2.Value = 1;
+        }
+
+    }
+}
